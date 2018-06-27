daf65f4cd246477015bc03c1a337cfce1f08fa9f
Jorge Rodríguez Castillo <kanh_2832@hotmail.com>
2018-06-27 15:44:57 -0500

    Creación de vista de detalle vacía

---
+++ app/src/main/java/pe/com/onecode/movieapp/ui/MovideDetailActivity.kt
@@ -0,0 +1,13 @@
+package pe.com.onecode.movieapp.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import pe.com.onecode.movieapp.R
+
+class MovideDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movide_detail)
+    }
+}
